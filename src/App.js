13e37fe997208e29d243f05d34a181d04fc3f4ca
varundubey771 <varundubey771@gmail.com>
2021-10-11 19:39:28 +0530

    local lottery contract for client side and web3 config

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+import web3 from "./web3";
+import lottery from "./lottery";
+
+class App extends Component {
+  state = { manager: "" };
+
+  async componentDidMount() {
+    const manager = await lottery.methods.manager().call();
+    this.setState({ manager });
+  }
+  render() {
+    return (
+      <div className="App">
+        <h2> Lottery contract manager {this.state.manager}</h2>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/lottery.js
@@ -0,0 +1,70 @@
+//setting local copy of contract that refers to correct address on rinkeby network
+//abstraction of contract for client side local js copy of contract
+import web3 from "./web3";
+
+const address = "0xf6157f79bfa9ec536aacf6ec8a0d0d509d56abf6";
+const abi = [
+  {
+    constant: true,
+    inputs: [],
+    name: "manager",
+    outputs: [{ name: "", type: "address" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: "pickWinner",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "random",
+    outputs: [{ name: "", type: "uint256" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "getPlayers",
+    outputs: [{ name: "", type: "address[]" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: "enter",
+    outputs: [],
+    payable: true,
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [{ name: "", type: "uint256" }],
+    name: "players",
+    outputs: [{ name: "", type: "address" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+];
+
+export default new web3.eth.Contract(abi, address);
+//exports complete copy of contract
